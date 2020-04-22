4d77a235a7c1832f5ae34a334c66b1278adf18f0
Chris Marshall <chris@codegoalie.com>
2020-04-21 20:04:57 -0400

    Change header text

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders headline", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = getByText(/how will you use your points/i);
+  expect(headerElement).toBeInTheDocument();
 });
