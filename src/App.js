ff298c367226da4353cc0a6a3faabe7ab01ca7f2
Chris Marshall <chris@codegoalie.com>
2020-04-21 20:20:52 -0400

    Replcae logo with placeholder inputs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>How will you use your points?</h1>
       </header>
+      <div className="App-inputs-container">
+        <input
+          type="text"
+          inputMode="numeric"
+          pattern="\d*"
+          name="points"
+          placeholder="Your points"
+        />
+        <input
+          type="date"
+          name="dates"
+          placeholder="Your dates"
+        />
+      </div>
     </div>
   );
 }
