116318f8e3c7c285ac906e8bab8a07278875ee9f
Chris Marshall <chris@codegoalie.com>
2020-06-07 06:44:36 -0400

    Make AvailabilityButton mobile responsive

--- src/AvailabilityButton.js
+++ src/AvailabilityButton.js
@@ -29,20 +29,32 @@ AvailabilityButton.propTypes = {
 export default AvailabilityButton;
 
 const Button = styled.a`
-  font-size: 1rem;
-  text-align: center;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 2rem;
+  width: 10rem;
+
+  margin-top: 0.5rem;
+  padding: 0.35rem 0.25rem 0.25rem;
+  border-radius: 0.5rem;
+  border: none;
+
   float: right;
+
+  font-size: 1rem;
+  text-decoration: none;
+
   background: darkslateblue;
   color: floralwhite;
-  border-radius: 0.5rem;
-  height: 2rem;
-  margin-top: 0.5rem;
-  width: 175px;
-  padding: 0.35rem 0.25rem 0.25rem;
-  border: none;
   box-shadow: 2px 1px 2px rgba(0, 0, 0, 0.25);
   transition: box-shadow 0.25s ease-in-out;
-  text-decoration: none;
+
+  @media (max-width: 768px) {
+    height: 3rem;
+    width: 100%;
+  }
 
   &:hover {
     box-shadow: 2px 1px 2px rgba(0, 0, 0, 0.5);
