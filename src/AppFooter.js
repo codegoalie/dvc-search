230fe9a28e509a5f7c05a5c2efef12b66ea6d541
Chris Marshall <chris@codegoalie.com>
2020-06-11 20:48:30 -0400

    Put goatcounter script in the HTML

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -7,34 +7,20 @@ const mailToHref = "mailto:chris@lineleader.io?subject=DVC%20Search";
 
 const AppFooter = props => {
   return (
-    <>
-      <script
-        async
-        data-uid="43dd8b2749"
-        src="https://lineleader.ck.page/43dd8b2749/index.js"
-      ></script>
-      <script
-        data-goatcounter="https://lineleader.goatcounter.com/count"
-        async
-        src="//gc.zgo.at/count.js"
-      ></script>
-      <Footer>
-        <HelpCallout>
-          Need help? Hit a snag? Email me:{" "}
-          <UnderlineLink href={mailToHref}>chris@lineleader.io</UnderlineLink>
-        </HelpCallout>
-        <p>&copy; {new Date().getFullYear()} LineLeader</p>
-        <p>
-          Crafted with care by{" "}
-          <UnderlineLink href="https://codegoalie.com">
-            CodeGoalie
-          </UnderlineLink>
-        </p>
-        <p style={{ marginTop: "0.75rem" }}>
-          LineLeader is in no way affiliated with the Walt Disney Company
-        </p>
-      </Footer>
-    </>
+    <Footer>
+      <HelpCallout>
+        Need help? Hit a snag? Email me:{" "}
+        <UnderlineLink href={mailToHref}>chris@lineleader.io</UnderlineLink>
+      </HelpCallout>
+      <p>&copy; {new Date().getFullYear()} LineLeader</p>
+      <p>
+        Crafted with care by{" "}
+        <UnderlineLink href="https://codegoalie.com">CodeGoalie</UnderlineLink>
+      </p>
+      <p style={{ marginTop: "0.75rem" }}>
+        LineLeader is in no way affiliated with the Walt Disney Company
+      </p>
+    </Footer>
   );
 };
 
