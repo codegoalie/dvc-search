f4931f5f4a74ea29251825a5fa67460527aceee9
Chris Marshall <chris@codegoalie.com>
2020-08-28 07:04:29 -0400

    Fix input widths on mobile

--- src/InputWrapper.js
+++ src/InputWrapper.js
@@ -4,6 +4,10 @@ const InputWrapper = styled.span`
   display: flex;
   flex-direction: column;
   font-family: "Overpass", sans-serif;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export default InputWrapper;
